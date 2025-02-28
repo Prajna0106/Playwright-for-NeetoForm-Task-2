623b2e9f5d7e4f529e3a087130369e5260d7ebfd
Prajna0106 <prajnajeet02@gmail.com>
2025-02-28 08:08:47 +0000

    Completely Updated the fixtures

--- e2e/fixtures/index.ts
+++ e2e/fixtures/index.ts
@@ -1,11 +1,10 @@
 import { test as base } from "@playwright/test";
 import LoginPage from "../poms/login";
-import Task1Page from "../poms/task1";
+import Task2Page from "../poms/task2";
 
 interface ExtendedFixtures {
   loginPage: LoginPage;
-
-  task1Page: Task1Page;
+  task2Page: Task2Page;
 }
 
 export const test = base.extend<ExtendedFixtures>({
@@ -14,8 +13,8 @@ export const test = base.extend<ExtendedFixtures>({
     await use(loginPage);
   },
 
-  task1Page: async ({ page }, use) => {
-    const task1Page = new Task1Page(page);
-    await use(task1Page);
+  task2Page: async ({ page, browser }, use) => {
+    const task2Page = new Task2Page(page, browser);
+    await use(task2Page);
   },
 });
